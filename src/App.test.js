4d74a446d757769156713eb8ced192bf596e9760
M Natoli <natoli.missy@gmail.com>
2022-03-22 11:24:09 -0500

    passing test with color button

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText('Learn React');
-  expect(linkElement).toBeInTheDocument();
+test("button has correct initial color", () => {
+    render(<App />);
+
+    //find an element with a role of button and text of 'Change to blue'
+    const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+    // expect the backgroung color to be red
+    expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+    //click button
+    fireEvent.click(colorButton);
+
+    //expect the backgroundColor to be blue
+    expect(colorButton).toHaveStyle({ backgroundColor: "blue"});
+
+    // expect the button text to be "Change to red"
+    expect(colorButton.textContent).toBe('Change to red');
 });
+
+
